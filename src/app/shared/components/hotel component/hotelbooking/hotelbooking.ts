82d54c51848a0649c2bc3fd82cd55ff27c346b0c
2hack1 <kapilagrawal230@gmail.com>
2025-08-21 18:08:27 +0530

    booking updated

--- src/app/shared/components/hotel component/hotelbooking/hotelbooking.ts
+++ src/app/shared/components/hotel component/hotelbooking/hotelbooking.ts
@@ -8,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class Hotelbooking {
 
+
+
+  // Modal visibility state 
+  showGuestsModal: boolean = false;
+  // Room values
+  rooms: number = 1;
+  maxRooms: number = 20;
+  // Displayed value on the <span>
+  totalGuests: number = this.rooms;
+  /** Toggle Guests Modal */
+  toggleGuestsModal() {
+    this.showGuestsModal = !this.showGuestsModal;
+  } /** Increment Rooms */
+  incRooms() {
+    if (this.rooms < this.maxRooms) {
+      this.rooms++; this.updateTotals();
+
+    }
+  } /** Decrement Rooms */
+  decRooms() {
+    if (this.rooms > 1
+
+    ) {
+      this.rooms--;
+      this.updateTotals();
+    }
+  } /** Apply Button */
+  applyGuests() {
+    this.updateTotals();
+    this.toggleGuestsModal();
+  } /** Update the displayed totals */
+  private updateTotals() {
+    this.totalGuests = this.rooms;
+  }
+
 }
